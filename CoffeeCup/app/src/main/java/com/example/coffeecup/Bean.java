7b67121ff04151576358d2d1a83f7731ca21889d
Saba Shaarbaftoosi <sabastoosi@gmail.com>
2020-11-08 12:33:36 -0600

    bean class done

--- CoffeeCup/app/src/main/java/com/example/coffeecup/Bean.java
+++ CoffeeCup/app/src/main/java/com/example/coffeecup/Bean.java
@@ -1,4 +1,38 @@
 package com.example.coffeecup;
 
 public class Bean {
+
+    private String mDarkness;
+    private String mName;
+    private String mRoastDate;
+
+    public Bean(String name, String darkness, String roastDate) {
+        mName = name;
+        mDarkness = darkness;
+        mRoastDate = roastDate;
+    }
+
+    public String getmDarkness() {
+        return mDarkness;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmRoastDate() {
+        return mRoastDate;
+    }
+
+    public void setmDarkness(String mDarkness) {
+        this.mDarkness = mDarkness;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public void setmRoastDate(String mRoastDate) {
+        this.mRoastDate = mRoastDate;
+    }
 }
