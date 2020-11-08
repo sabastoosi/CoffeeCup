c9dc8b1e62b515b343cfca7c38b1252ca7c0edc9
CJ Olsen <cjols97@iastate.edu>
2020-11-08 12:46:10 -0600

    recycler view init

---
+++ CoffeeCup/app/src/main/java/com/example/coffeecup/RecyclerAdapter.java
@@ -0,0 +1,45 @@
+package com.example.coffeecup;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    private final String[] mDataset;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public MyViewHolder(TextView v) {
+            super(v);
+            textView = v;
+        }
+    }
+
+    public RecyclerAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.MyViewHolder holder, int position) {
+        holder.textView.setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
