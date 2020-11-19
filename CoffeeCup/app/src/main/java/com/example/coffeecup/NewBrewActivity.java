30983fcf5c74df782f3339f70524f2ef30ed07a4
CJ Olsen <cjols97@iastate.edu>
2020-11-19 15:43:19 -0600

    post brew getExtra

--- CoffeeCup/app/src/main/java/com/example/coffeecup/NewBrewActivity.java
+++ CoffeeCup/app/src/main/java/com/example/coffeecup/NewBrewActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -71,6 +72,7 @@ public class NewBrewActivity extends AppCompatActivity {
 
         // go to main list of brews
         Intent intent = new Intent(this, PostBrewActivity.class);
+        intent.putExtra("Brew name", brew.getmName());
         startActivity(intent);
     }
 
--- CoffeeCup/app/src/main/java/com/example/coffeecup/PostBrewActivity.java
+++ CoffeeCup/app/src/main/java/com/example/coffeecup/PostBrewActivity.java
@@ -3,12 +3,25 @@ package com.example.coffeecup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PostBrewActivity extends AppCompatActivity {
+
+    Brew brew;
+    ListOfBrews listOfBrews = new ListOfBrews();
+
+    TextView brewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_brew);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle.getString("Brew name") != null) {
+            brew = ListOfBrews.find(bundle.getString("Brew name"));
+        }
+
+        brewName = findViewById(R.id.post_brew_name);
     }
 }
